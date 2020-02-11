dc72d66285b7bcbf67f6d03488286f906cf5aa74
costaarthur <costaarthur@hotmail.com>
2020-02-11 16:55:02 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
+import './styles.css';
 
 import TechList from './components/TechList'
+import Header from './components/Header'
+import PostList from './components/PostList'
+import Menu from './components/Menu'
 
 function App() {
-  //return <h1> Hail meu Clã</h1>
-  return <TechList />
+  return (
+    <div className="App">
+      <Header />
+
+      <TechList />
+    </div>
+
+    // <div className="header">
+    //   <Header />
+    // </div>
+  )
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+
+// import Header from './components/Header'
+
+// const App = () => (
+//   <div className="App">
+//     <Header />
+//   </div>
+// );
+
+// export default App
---
+++ src/components/Footer/Index.js
@@ -0,0 +1,15 @@
+"use strict"
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer text-center">
+        <div className="container">
+          <p className="footer-text">Copyright 2020 Arthur Costa. All rights reserved</p>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Footer
